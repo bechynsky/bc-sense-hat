7855861b94c8a5c2f73ced55dd380479f5c7320f
Stepan Bechynsky <stepan@bechynsky.cz>
2019-01-20 16:41:05 +0100

    Support for Sense HAT

--- bc-sense-hat.py
+++ bc-sense-hat.py
@@ -1,5 +1,8 @@
 import paho.mqtt.subscribe as subscribe
+from sense_hat import SenseHat
 
+# Sense HAT
+sense = SenseHat()
 
 # Change this based on your devices connected to gateway
 custom_names = {
@@ -15,14 +18,16 @@ def on_message_print(client, userdata, message):
     # Message parser
     if (len(topic_parts) != 5) or (topic_parts[1] not in custom_names):
         return
-        
+
     if (topic_parts[4] != "temperature"):
         return
 
+    temperature =  float(message.payload)
+    print(temperature)
     print(custom_names[topic_parts[1]] + ": " + message.payload.decode("ascii") + " °C")
-    
+    sense.show_message(custom_names[topic_parts[1]] + ": " + str(int(temperature)))
 
 # Change hostname to reflect your setup
 # If you run it on local computer use "localhost"
-subscribe.callback(on_message_print, "#", hostname="192.168.100.200")
+subscribe.callback(on_message_print, "#", hostname="localhost")
 
